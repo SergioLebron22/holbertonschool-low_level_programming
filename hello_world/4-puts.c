0d761afc9876e0e97fd2a6f5641b377be969f7f7
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-19 23:23:05 -0400

    Fixed puts c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int main()
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	char string[] = "Programming is like building a multilingual puzzle \n";
-	puts(string);
-
-	return 0;
-} 
-
+	char sring[] = "\"Programming is like building a multilingual puzzle \n";
+	puts(string);	
+	return (0);
+}
