83b6a9c6eeb27af968cbf1c9bb900d04972f4c5e
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-21 21:36:08 -0400

    Added print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_to_98 - will print all natural numbers from n to 98
+ *
+ * @n: startring point
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >=98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
+}
