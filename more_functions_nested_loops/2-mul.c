903937b5dfd664702be9fd108cdc802171ff8319
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-22 10:22:21 -0400

    Added mul function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - will multiply the two given numbers
+ *
+ * @a: arg number to be multiplied
+ *
+ * @b: arg number to be multiplied
+ *
+ * Return: product of the two args
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
