4492d007e9ca8b26ff758e9df7aa34fd75c4da41
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-22 14:44:52 -0400

    Fixed print lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -14,10 +14,12 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-
-	for (i = 0; i < n; i++)
+	else
 	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
