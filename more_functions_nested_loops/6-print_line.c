cde8ca5a948da24d1458bb72c47153af92e984f0
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-22 14:23:39 -0400

    Added print lines function

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - will draw a linein the terminal
+ *
+ * @n: arg length of the line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0)
+		_putchar('\n');;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
