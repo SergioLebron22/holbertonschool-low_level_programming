bb4ae678599347c00778e185894b3dfe6269ec8a
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-21 21:21:10 -0400

    added add function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: arg int to be added
+ *
+ * @b: arg int to be added
+ *
+ * Return: Result of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
