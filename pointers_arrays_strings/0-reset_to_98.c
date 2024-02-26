5a5ce14e299bb5205fbc629d2ec6a6480ae94753
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-26 09:51:50 -0400

    added reset to 98 function

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - will update the the pointer is pointinf to,
+ * to 98
+ *
+ * @n: pointer to be updated
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
