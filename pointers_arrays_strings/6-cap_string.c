33519c280f3e91362211ab127186f0b62b6589f3
SergioLebron22 <lebronsergio22@gmail.com>
2024-02-28 19:03:43 -0400

    Changes to cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,12 +11,12 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i;
 	int counter = 0;
 	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + counter) >= 97 && (*s + counter) <= 122)
-		*(s + counter) = *(s + counter - 32);
+	if (*(s + counter) >= 97 && *(s + counter) <= 122)
+		*(s + counter) = *(s + counter) - 32;
 	counter++;
 
 	while (*(s + counter) != '\0')
@@ -33,4 +33,4 @@ char *cap_string(char *s)
 		counter++;
 	}
 	return (s);
-	}
+}
