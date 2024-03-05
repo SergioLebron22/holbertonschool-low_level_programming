33f2bd9a9179ed357412d45ca1f090ddc434cb1f
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-05 10:53:07 -0400

    Added whats my name function

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - will print its name
+ *
+ * @argc - arg count
+ * @argv - arg vector
+ *
+ * Return: command name
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
