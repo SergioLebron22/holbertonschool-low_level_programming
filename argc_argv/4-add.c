5c336ca4d764d26cbddd053e6d4940d9ab244ddf
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-05 14:46:27 -0400

    Added add function

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - will add positive numbers
+ *
+ * @argc: counter
+ * @argv: vector
+ *
+ * Return: sum of numbers
+ */
+
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int i, j;
+	char *num;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+
+	for (i = 1; i < argc; i++)
+	{
+		num = argv[i];
+		for (j = 0; num[j] != '\0'; j++)
+		{
+			if (!isdigit(num[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
