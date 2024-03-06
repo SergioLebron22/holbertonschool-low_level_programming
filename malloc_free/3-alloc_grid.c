02a59fc069eb7d683f8778c5bdbf998195e2fb30
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-06 16:39:41 -0400

    added alloc grid function

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - will return a pointer to a 2 dimensional
+ * array of integers
+ *
+ * @width: row of matrix
+ * @height: columns of string
+ *
+ * Return: pointer to matrix or null
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int i, j, l;
+	int *p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		*(matrix + i) = (int *)malloc(width * sizeof(int));
+		if (*(matrix + i) == NULL)
+		{
+			for (j = 0; j < height; j++)
+			{
+				p = matrix[j];
+				free(p);
+			}
+			free(matrix);
+			return (NULL);
+		}
+	}
+	for (l = 0; l < height; l++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[l][j] = 0;
+		}
+	}
+	return (matrix);
+}
+
+
