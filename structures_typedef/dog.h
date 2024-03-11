ce89b54b463737c47c6641f8ba18a7ab27d24f60
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-11 09:12:52 -0400

    Added dog header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - dog
+ *
+ * @name: name if dog
+ * @owner: owner of dog
+ * @age: age of dog
+ *
+ * Description: this is a dog
+ */
+
+struct dog
+{
+  char *name;
+  char *owner;
+  float age;
+};
