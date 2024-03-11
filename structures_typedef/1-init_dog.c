987e6cad93cca36de970a6209db6cef512c50394
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-11 10:42:35 -0400

    Added dog constructor

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - will initialize a dog structure
+ *
+ * @d: dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
