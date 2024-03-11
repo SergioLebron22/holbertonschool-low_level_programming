7bcdf8e91f6287ddffbedb5491a8b40f381391dc
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-11 11:25:18 -0400

    Added print dog function

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - will print a dog
+ *
+ * @d: given dog
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL || d->owner == NULL)
+	{
+		printf("nill");
+		return;
+	}
+
+	printf("NAME: %s\nAGE: %f\nOWNER: %s\n", d->name, d->age, d->owner);
+}
