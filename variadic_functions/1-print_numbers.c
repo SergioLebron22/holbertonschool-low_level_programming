e9070fa3ca5bc2b5bf81a222c30ce5e0edd44193
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-14 12:55:14 -0400

    fixed bug in print numbers function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,16 +19,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-				printf("%d%s", va_arg(args, int), separator);
-			else
-				printf("%d", va_arg(args, int));
-		}
-		printf("\n");
+		if (i != n - 1 && separator != NULL)
+			printf("%d%s", va_arg(args, int), separator);
+		else
+			printf("%d", va_arg(args, int));
 	}
+	printf("\n");
 	va_end(args);
 }
