8ccb4b4f8a7caba49e0faef6637e1305d4fdf7c2
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-25 12:38:28 -0400

    Added print list function

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - will print contents of singly
+ * linked list
+ *
+ * @h: head of list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)");
+
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
