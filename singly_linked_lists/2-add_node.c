adb9799d097e73360b54305392d0dfa1c66d464d
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-25 13:43:26 -0400

    Added add node function

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+int _strlenRecursive(char *str);
+
+/**
+ * add_node - will add node to the beginning of a list
+ *
+ * @head: head of the list
+ * @str: string to be added
+ *
+ * Return: the address of the new element, NULL if it fails
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return(NULL);
+	}
+	
+	new->len = _strlenRecursive(new->str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * strlenRecursive - returns the length of a string
+ * @str: string
+ * Return: length of string
+ */
+
+int _strlenRecursive(char *str)
+{
+	if (*str == 0)
+		return (0);
+	else
+		return (1 + _strlenRecursive(str + 1));
+}
