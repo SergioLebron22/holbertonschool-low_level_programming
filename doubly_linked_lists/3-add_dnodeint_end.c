d6b9e1a061ae554343494ff3e1ff03683a5839ff
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-27 11:50:07 -0400

    fixing dnode add to end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,8 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *aux = *head;
+	dlistint_t *new;
+    dlistint_t *aux = *head;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -24,9 +25,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while (aux->next)
 			aux = aux->next;
 		aux->next = new;
+        new->prev = aux;
 	}
 	else
 		*head = new;
 
-	return (new);
+	return (&new);
 }
