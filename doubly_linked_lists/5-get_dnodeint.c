327456676e8e4d959771309e555ff98b2fc1f707
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-27 13:23:07 -0400

    fixed betty

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,29 +1,33 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index
+ * get_dnodeint_at_index - will get the nth node in the list
+ *
+ * @head: start of list
+ * @index: position in list
+ *
+ * Return: node at index, NULL if it does not exist
 */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i;
-    dlistint_t *node;
+	unsigned int i;
+	dlistint_t *node;
 
-    i = 0;
-    node = head;
+	i = 0;
+	node = head;
 
-    while (i < index)
-    {
-        if(!node)
-        {
-            return (NULL);
-        }
-        else
-        {
-            node = node->next;
-            i++;
-        }
-    }
-
-    return (node);
+	while (i < index)
+	{
+		if (!node)
+		{
+			return (NULL);
+		}
+		else
+		{
+			node = node->next;
+			i++;
+		}
+	}
+	return (node);
 }
