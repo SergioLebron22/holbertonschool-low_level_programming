8cf99140bb0164ad8946f3cb17eaae37a012e794
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-27 09:18:32 -0400

    fixing add dnode

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,8 +19,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	if (*head != NULL)
+		(*head)->prev = new;
+		
 	new->n = n;
 	new->next = *head;
+	new->prev = NULL;
 	*head = new;
 
 	return (new);
