2fe453a46d7638e86ff7e8aa3d468dd64b26df3a
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-28 13:03:03 -0400

    Added append text to file function

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * 
+ * 
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int file;
+    int len = 0;
+    int inlen = 0;
+    char *str;
+
+    if (filename == NULL)
+    {
+        return (-1);
+    }
+
+    file = open(filename, O_WRONLY | O_APPEND);
+
+    if (file == -1)
+    {
+        return (-1);
+    }
+
+    if (text_content != NULL)
+    {
+        for (inlen = 0, str = text_content; *str; str++)
+        {
+            inlen++;
+        }
+        len = write(file, text_content, inlen);
+    }
+
+    if (close(file) == -1 || inlen != len)
+        return (-1);
+
+    return (1);
+}
+
