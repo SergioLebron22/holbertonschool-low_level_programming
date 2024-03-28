7bed4a0ff0391c24cc5be291f846d27a45637b72
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-27 23:45:14 -0400

    fixing insert node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,16 +11,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+        
+	new = malloc(sizeof(dlistint_t));
 
 	for (i = 0; i < idx - 1; i++)
 	{
+		if (new == NULL)
+			return (NULL);
 		temp = temp->next;
     }
 
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 		return (NULL);
