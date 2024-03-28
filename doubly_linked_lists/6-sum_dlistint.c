4e12d436f4d2c383d33675c38fd0fac49f851dae
SergioLebron22 <lebronsergio22@gmail.com>
2024-03-27 22:11:41 -0400

    Fixed betty

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,24 @@
 #include "lists.h"
 
 /**
- * 
- * 
+ * sum_dlistint - will sum all integer values in the list
+ *
+ * @head: start of list
+ *
+ * Return: total of sum
 */
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    if (head == NULL)
-        return (0);
+	if (head == NULL)
+		return (0);
 
-    while (head != NULL)
-    {
-        sum = sum + head->n;
-        head = head->next;
-    }
-    return (sum);
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
 }
